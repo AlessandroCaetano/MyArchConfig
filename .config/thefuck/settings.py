b49123c52a2fd5ee2dcc27ef50e554a1856b44f0
Alessandro Caetano <alessandro.caetanob@gmail.com>
2016-09-27 11:07:46 -0400

    Updating config files

---
+++ .config/thefuck/settings.py
@@ -0,0 +1,20 @@
+# The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+
+# env = {'LANG': 'C', 'LC_ALL': 'C', 'GIT_TRACE': '1'}
+# require_confirmation = True
+# alter_history = True
+# rules = [<const: All rules enabled>]
+# no_colors = False
+# exclude_rules = []
+# priority = {}
+# debug = False
+# wait_command = 3
+# history_limit = None
